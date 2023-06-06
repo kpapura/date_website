5312ea6b828935e3856a702211e5ea1e445cfd67
kpapura <125825500+kpapura@users.noreply.github.com>
2023-06-06 16:49:08 +0300

    simple template

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// Інтерфейс користувача
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.getElementById('openModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+});
+
+// Код для взаємодії з сервером
+// Ви можете використовувати AJAX або Fetch API для взаємодії з сервером та обробки запитів клієнтів.
